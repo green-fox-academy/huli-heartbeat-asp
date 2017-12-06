26d187188dba2ae91bd8ca8b0328a3e963aaab78
bkorbuly <b.korbuly@gmail.com>
2017-12-06 15:25:42 +0100

    Middleware init

---
+++ HeartBeat/HeartBeat/NagiosMiddlewareExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeartBeat
+{
+    public static class NagiosMiddlewareExtension
+    {
+        public static IApplicationBuilder UseNagios(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NagiosMiddleware>();
+        }
+    }
+}
