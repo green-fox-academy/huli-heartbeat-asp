903f01b8fe320b8b5353ca39c27d36ef37e66d74
bkorbuly <b.korbuly@gmail.com>
2017-12-07 13:38:08 +0100

    Enpoint is reachable and it returns a JSON

---
+++ HeartBeat/HeartBeat/HeartBeatMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HeartBeat
+{
+    public class HeartBeatMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private static readonly PathString path = new PathString("/heartbeat");
+
+        public HeartBeatMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.Equals(path))
+            {
+                await CheckStatus(httpContext);
+                return;
+            }
+            await _next(httpContext);
+        }
+
+        public Task CheckStatus(HttpContext httpContext)
+        {
+            var content = JsonConvert.SerializeObject("cup");
+            return httpContext.Response.WriteAsync(content);
+        }
+    }
+}
