6ccac349b0e530f8994e52a6ac229dd6151dbf3a
csibvili <csibi.vili@gmail.com>
2018-01-03 13:42:35 +0100

    delete commented parts and unnecessary usings

--- HeartBeat/HeartBeat/ServerStatus.cs
+++ HeartBeat/HeartBeat/ServerStatus.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HeartBeat
+﻿namespace HeartBeat
 {
     public class ServerStatus
     {
--- HeartBeat/HeartBeatTest/HeartBeat_Test.cs
+++ HeartBeat/HeartBeatTest/HeartBeat_Test.cs
@@ -1,24 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using System;
-using System.Net.Http;
-using Xunit;
-using System.Web;
-using Moq;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using HeartBeat;
+using Microsoft.AspNetCore.Http;
+using Moq;
 using System.IO;
+using Xunit;
 
 namespace HeartBeatTest
 {
     public class HttpResponse_Test
     {
-        //public Mock<HttpContext> contextMock { get; set; }
-        //public Mock<HttpRequest> requestMock { get; set; }
-        //public Mock<HeartBeatMiddleware> middlewareMock { get; set; }
-
         [Fact]
         public void Test()
         {
@@ -36,33 +25,8 @@ namespace HeartBeatTest
             var middlewareMock = new Mock<HeartBeatMiddleware>();
             //Act
             var result = httpRequestMock.Object.Path;
-            //middlewareMock.Invoke(httpContextMock.Object).Wait();
             //Assert
-            //middlewareMock.Verify(x => x.Invoke(httpContextMock.Object));
             Assert.Equal("/heartbeat", result);
-
-
-            //Arrange
-            //Mock<HttpContext> contextMock;
-            //Mock<HttpRequest> requestMock = new Mock<HttpRequest>();
-            //middlewareMock = new Mock<HeartBeatMiddleware>();
-            //requestMock.Setup(x => x.Path).Returns(new PathString("/heartbeat"));
-            //contextMock = new Mock<HttpContext>();
-            //contextMock.SetupAllProperties();
-            //var delgateMock = new Mock<RequestDelegate>();
-            //contextMock.Setup(x => x.Request).Returns(requestMock.Object);
-
-            //(next: (innerHttpContext) => Task.FromResult(0));
-            //var HeartBeatMiddlewareMock = new HeartBeat.HeartBeatMiddleware(delgateMock.Object);
-            //Act
-            //await HeartBeatMiddlewareMock.Invoke(contextMock.Object);
-
-            //var responseJson = HeartBeatMiddlewareMock.Invoke(contextMock.Object);
-            //var responseJson2 = responseJson.ToString();
-            //Assert
-            //Assert.Equal("{\"HttpStatus\":200}", contextMock.Object.Request.Path);
-            //middlewareMock.Verify(x => HeartBeatMiddlewareMock.Invoke(contextMock.Object));
-            //contextMock.Verify(x => x. == "/heartbeat");
         }
     }
 }
