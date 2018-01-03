a5506de7efb0b7b1ca031a99eab043a1cacbe2fe
csibvili <csibi.vili@gmail.com>
2018-01-03 16:58:48 +0100

    empty constructor + constructor with connectionstring

--- HeartBeat/HeartBeat/HeartBeatMIddlewareExtension.cs
+++ HeartBeat/HeartBeat/HeartBeatMIddlewareExtension.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 
 namespace HeartBeat
 {
     public static class HeartBeatMiddlewareExtension
     {
-        public static IApplicationBuilder UseHeartBeat(this IApplicationBuilder builder, DbContext dbContext)
+        public static IApplicationBuilder UseHeartBeat(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<HeartBeatMiddleware>();
+        }
+
+        public static IApplicationBuilder UseHeartBeat(this IApplicationBuilder builder, string connectionString)
+        {
+            return builder.UseMiddleware<HeartBeatMiddleware>(connectionString);
         }
     }
 }
