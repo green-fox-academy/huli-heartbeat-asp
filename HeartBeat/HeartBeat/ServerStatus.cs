8d2b9404971e1c8fa70adf83a10bffb59fe31055
csibvili <csibi.vili@gmail.com>
2018-01-03 17:00:04 +0100

    move CheckDbStatus() here + use try catch

--- HeartBeat/HeartBeat/ServerStatus.cs
+++ HeartBeat/HeartBeat/ServerStatus.cs
@@ -1,4 +1,7 @@
-﻿namespace HeartBeat
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace HeartBeat
 {
     public class ServerStatus
     {
@@ -9,5 +12,28 @@
         }
         public int HttpStatus { get; }
         public bool DbStatus { get; }
+
+        public static bool CheckDbStatus(string connectionString)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    System.Diagnostics.Debug.WriteLine(connection.Database.Length);
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+                        command.CommandText = "SELECT 1";
+                        command.ExecuteScalar();
+                        return true;
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
     }
 }
